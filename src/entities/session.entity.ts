a628eeddb4174166ff8903472e3125334e20d432
min <min@airfolc.co.jp>
2022-12-19 09:26:30 +0900

    add session table

---
+++ src/entities/session.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  BeforeInsert,
+} from 'typeorm';
+
+@Entity('mock_session')
+export class SessionEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  session_id: string;
+
+  @Column({ type: 'int' })
+  customer_id: string;
+
+  @Column({ type: 'datetime', default: () => "NOW() + INTERVAL '24 hours'" })
+  expire_datetime: Date;
+
+  @BeforeInsert()
+  expireDateTime() {
+    this.expire_datetime = new Date(
+      this.expire_datetime.getTime() + 24 * 60 * 60 * 1000,
+    );
+  }
+}
