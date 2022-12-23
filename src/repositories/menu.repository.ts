b16ae8fc2e0e631ab86654ad163f47b73f8ab02a
min <min@airfolc.co.jp>
2022-12-23 10:29:47 +0900

    add implement get list of menu on  MenuRepository

--- src/repositories/menu.repository.ts
+++ src/repositories/menu.repository.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { MenuEntity } from '../entities/menu.entity';
 import { Menu } from '../model/menu';
-import { CustomerId } from './../model/customer-id';
+import { MenuId } from '../model/menu-id';
 
 @Injectable()
 export class MenuRepository {
@@ -16,7 +16,20 @@ export class MenuRepository {
     this.sessionRepository.insert(menuEntity);
   }
 
-  async getListOf(customerId: CustomerId): Promise<Menu[]> {
-    return;
+  async getMenuListOf(course_id: number): Promise<Menu[]> {
+    const menusEntity: MenuEntity[] = await this.sessionRepository.findBy({
+      course_id,
+    });
+    if (menusEntity.length === 0) return [Menu.empty()];
+    return menusEntity.map((menuEntity) => this.toMenu(menuEntity));
+  }
+
+  toMenu(menuEntity: MenuEntity): Menu {
+    return new Menu(
+      new MenuId(menuEntity.menu_id),
+      menuEntity.menu_name,
+      menuEntity.menu_type,
+      menuEntity.price,
+    );
   }
 }
