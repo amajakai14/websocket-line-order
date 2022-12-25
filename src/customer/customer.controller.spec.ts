4880659956e10aa6c69dfbf936dfea12cd93c6f8
Means <amajakai14@gmail.com>
2022-12-25 09:58:28 +0900

    test: add providers on customer

--- src/customer/customer.controller.spec.ts
+++ src/customer/customer.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CustomerRepository } from '../repositories/customer.repository';
 import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
 
 describe('CustomerController', () => {
   let controller: CustomerController;
@@ -7,6 +9,7 @@ describe('CustomerController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomerController],
+      providers: [CustomerService, CustomerRepository],
     }).compile();
 
     controller = module.get<CustomerController>(CustomerController);
