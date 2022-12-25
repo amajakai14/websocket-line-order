348a7f20efb709818e0e8b7d65986a1f3033fdbb
Means <amajakai14@gmail.com>
2022-12-25 18:32:11 +0900

    import TypeOrmEntity

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerEntity } from '../entities/customer.entity';
 import { CustomerRepository } from '../repositories/customer.repository';
 import { CustomerController } from './customer.controller';
 import { CustomerService } from './customer.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CustomerEntity])],
   controllers: [CustomerController],
   providers: [CustomerService, CustomerRepository],
 })
